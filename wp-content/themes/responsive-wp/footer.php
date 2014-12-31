46c8e6fda931b30c970086d1d5e87bbe2a37a9fb
Tracy Rotton <tracy@taupecat.com>
2014-12-31 17:04:58 -0500

    Latest version of Underscores; moving the JS and Sass directories into the theme directory

--- wp-content/themes/responsive-wp/footer.php
+++ wp-content/themes/responsive-wp/footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'responsive-wp' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'responsive-wp' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'responsive-wp' ), 'Responsive WP', '<a href="http://taupecat.com" rel="designer">Tracy Rotton</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'responsive-wp' ), 'Responsive WP', '<a href="http://responsivewptheming.com" rel="designer">Tracy Rotton</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- wp-content/themes/responsive-wp/inc/extras.php
+++ wp-content/themes/responsive-wp/inc/extras.php
@@ -8,18 +8,6 @@
  */
 
 /**
- * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
- *
- * @param array $args Configuration arguments.
- * @return array
- */
-function responsive_wp_page_menu_args( $args ) {
-	$args['show_home'] = true;
-	return $args;
-}
-add_filter( 'wp_page_menu_args', 'responsive_wp_page_menu_args' );
-
-/**
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
@@ -35,7 +23,7 @@ function responsive_wp_body_classes( $classes ) {
 }
 add_filter( 'body_class', 'responsive_wp_body_classes' );
 
-if ( ! function_exists( '_wp_render_title_tag' ) ) :
+if ( version_compare( $GLOBALS['wp_version'], '4.1', '<' ) ) :
 	/**
 	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
 	 *
@@ -67,9 +55,7 @@ if ( ! function_exists( '_wp_render_title_tag' ) ) :
 		return $title;
 	}
 	add_filter( 'wp_title', 'responsive_wp_wp_title', 10, 2 );
-endif;
 
-if ( ! function_exists( '_wp_render_title_tag' ) ) :
 	/**
 	 * Title shim for sites older than WordPress 4.1.
 	 *
@@ -77,28 +63,9 @@ if ( ! function_exists( '_wp_render_title_tag' ) ) :
 	 * @todo Remove this function when WordPress 4.3 is released.
 	 */
 	function responsive_wp_render_title() {
-		echo '<title>' . wp_title( '|', false, 'right' ) . "</title>\n";
+		?>
+		<title><?php wp_title( '|', true, 'right' ); ?></title>
+		<?php
 	}
 	add_action( 'wp_head', 'responsive_wp_render_title' );
 endif;
-
-/**
- * Sets the authordata global when viewing an author archive.
- *
- * This provides backwards compatibility with
- * http://core.trac.wordpress.org/changeset/25574
- *
- * It removes the need to call the_post() and rewind_posts() in an author
- * template to print information about the author.
- *
- * @global WP_Query $wp_query WordPress Query object.
- * @return void
- */
-function responsive_wp_setup_author() {
-	global $wp_query;
-
-	if ( $wp_query->is_author() && isset( $wp_query->post ) ) {
-		$GLOBALS['authordata'] = get_userdata( $wp_query->post->post_author );
-	}
-}
-add_action( 'wp', 'responsive_wp_setup_author' );
