4ac80250f1bc623d26d6354cde73a9fd76b5d65c
Tracy Rotton <tracy@taupecat.com>
2015-01-24 00:02:48 -0500

    Header

--- wp-content/themes/responsive-wp/header.php
+++ wp-content/themes/responsive-wp/header.php
@@ -22,15 +22,27 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'responsive-wp' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'responsive-wp' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="block">
+
+			<img src="http://placehold.it/320x160">
+
+			<div class="site-logo">
+				<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logo.png">
+			</div>
+
+			<div class="site-branding">
+				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</div><!-- .site-branding -->
+
+		</a>
+
 	</header><!-- #masthead -->
+
+	<nav id="site-navigation" class="main-navigation" role="navigation">
+		<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'responsive-wp' ); ?></button>
+		<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+	</nav><!-- #site-navigation -->
 
 	<div id="content" class="site-content">
