63917cf5232d671e053ddf211d1c44d55527481c
Tracy Rotton <tracy@taupecat.com>
2015-02-04 22:24:07 -0500

    finishing up the footer.

--- wp-content/themes/responsive-wp/footer.php
+++ wp-content/themes/responsive-wp/footer.php
@@ -13,8 +13,6 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'responsive-wp' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'responsive-wp' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'responsive-wp' ), 'Responsive WP', '<a href="http://responsivewptheming.com" rel="designer">Tracy Rotton</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
